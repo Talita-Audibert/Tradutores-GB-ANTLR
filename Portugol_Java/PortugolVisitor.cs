c5e810cb869d69cc8d218f5c8fa36956640ac82e
Unknown <mateusbpt1@gmail.com>
2016-11-21 18:22:19 -0200

    Adicionar fechamento de chaves para classe.

--- Portugol_Java/PortugolVisitor.cs
+++ Portugol_Java/PortugolVisitor.cs
@@ -6,146 +6,159 @@ using System.Text;
 
 namespace Portugol_Java
 {
-	public static class Identacao
-	{
-		public const string Metodo = "\t";
-		public const string CorpoMetodo = "\t\t";
-	}
-	public class PortugolVisitor : PortugolBaseVisitor<int>
-	{
-		StringBuilder classFile;
+    public static class Identacao
+    {
+        public const string Metodo = "\t";
+        public const string CorpoMetodo = "\t\t";
+    }
+    public class PortugolVisitor : PortugolBaseVisitor<int>
+    {
+        StringBuilder classFile;
+        int contadorLinhas = 0;
 
-		public PortugolVisitor()
-		{
-			classFile = new StringBuilder();
-		}
+        public PortugolVisitor()
+        {
+            classFile = new StringBuilder();
+        }
 
-		public void SaveToFile(string filePath)
-		{
-			if (classFile != null)
-			{
-				using (var file = new StreamWriter(filePath, false))
-				{
-					file.Write(classFile.ToString());
-				}
-			}
-		}
+        public string ChavesFinais(int contador)
+        {
+            var auxiliar = "";
 
-		public override int VisitPrograma([NotNull] PortugolParser.ProgramaContext context)
-		{
-			string nomeClasse = context.ID().GetText();
-			classFile.Append($"public class {nomeClasse}{{ {Environment.NewLine}{Identacao.Metodo} public static void main(String[] args){{ {Environment.NewLine} ");
+            for (int i = 0; i < contador; i++)
+            {
+                auxiliar += Environment.NewLine + "}";
+            }
 
-			//sempre instancia uma variavel de IO mesmo se não é usado.
-			//Se for instanciado no visitor das funções é sempre criado uma instancia nova
-			classFile.Append($"{ Identacao.CorpoMetodo}Scanner entrada = new Scanner(System.in);{Environment.NewLine}");
+            return auxiliar;
 
-			return base.VisitPrograma(context);
-		}
+        }
 
-		public override int VisitDecFuncoes([NotNull] PortugolParser.DecFuncoesContext context)
-		{
-			string metodos = context.ID().ToString();
+        public void SaveToFile(string filePath)
+        {
+            if (classFile != null)
+            {
+                using (var file = new StreamWriter(filePath, false))
+                {
+                    file.Write(classFile.ToString()+ChavesFinais(contadorLinhas));
+                }
+            }
+        }
 
-			switch (metodos)
-			{
-				case "escreval":
-					classFile.Append($"{Identacao.CorpoMetodo}System.out.println();{Environment.NewLine}");
-					break;
+        public override int VisitPrograma([NotNull] PortugolParser.ProgramaContext context)
+        {
+            string nomeClasse = context.ID().GetText();
+            classFile.Append($"public class {nomeClasse}{{ {Environment.NewLine}{Identacao.Metodo} public static void main(String[] args){{ {Environment.NewLine} ");
+            //sempre instancia uma variavel de IO mesmo se não é usado.
+            //Se for instanciado no visitor das funções é sempre criado uma instancia nova
+            classFile.Append($"{ Identacao.CorpoMetodo}Scanner entrada = new Scanner(System.in);{Environment.NewLine}");
+            contadorLinhas += 2;
+            return base.VisitPrograma(context);
+        }
 
-				case "escreva":
-					string texto = context.listaPar().STRING(0).ToString();
-					classFile.Append($"{Identacao.CorpoMetodo}System.out.println({texto});{Environment.NewLine}");
-					break;
+        public override int VisitDecFuncoes([NotNull] PortugolParser.DecFuncoesContext context)
+        {
+            string metodos = context.ID().ToString();
 
-				case "leia":
-					string nomeVar = context.listaPar().ID(0).ToString();
-					classFile.Append($"{Identacao.CorpoMetodo}{nomeVar} = entrada.next();{Environment.NewLine}");
-					break;
-			}
-			
-			return base.VisitDecFuncoes(context);
-		}
+            switch (metodos)
+            {
+                case "escreval":
+                    classFile.Append($"{Identacao.CorpoMetodo}System.out.println();{Environment.NewLine}");
+                    break;
 
-		public override int VisitListaVar([NotNull] PortugolParser.ListaVarContext context)
-		{
-			string tipo = context.tipo().GetText();
-			string variavel = context.ID(0).ToString();
-			classFile.Append($"{Identacao.CorpoMetodo}");
+                case "escreva":
+                    string texto = context.listaPar().STRING(0).ToString();
+                    classFile.Append($"{Identacao.CorpoMetodo}System.out.println({texto});{Environment.NewLine}");
+                    break;
 
-			switch (tipo)
-			{
-				case "REAL":
-					classFile.AppendFormat("double {0}", variavel);
-					break;
-				case "INTEIRO":
-					classFile.AppendFormat("int {0}", variavel);
-					break;
-				case "BOOLEANO":
-					classFile.AppendFormat("bool {0}", variavel);
-					break;
-				case "LOGICO":
-					classFile.AppendFormat("bool {0}", variavel);
-					break;
-				case "STRING":
-					classFile.AppendFormat("string {0}", variavel);
-					break;
-				case "CARACTERE":
-					classFile.AppendFormat("char {0}", variavel);
-					break;
-			}
+                case "leia":
+                    string nomeVar = context.listaPar().ID(0).ToString();
+                    classFile.Append($"{Identacao.CorpoMetodo}{nomeVar} = entrada.next();{Environment.NewLine}");
+                    break;
+            }
 
-			int r = context.ID().Length;
+            return base.VisitDecFuncoes(context);
+        }
 
-			for (int i = 1; i < r; i++)
-			{
-				variavel = context.ID(i).ToString();
-				classFile.AppendFormat(", {0}", variavel);
-			}
+        public override int VisitListaVar([NotNull] PortugolParser.ListaVarContext context)
+        {
+            string tipo = context.tipo().GetText();
+            string variavel = context.ID(0).ToString();
+            classFile.Append($"{Identacao.CorpoMetodo}");
 
-			classFile.Append(";\n");
+            switch (tipo)
+            {
+                case "REAL":
+                    classFile.AppendFormat("double {0}", variavel);
+                    break;
+                case "INTEIRO":
+                    classFile.AppendFormat("int {0}", variavel);
+                    break;
+                case "BOOLEANO":
+                    classFile.AppendFormat("bool {0}", variavel);
+                    break;
+                case "LOGICO":
+                    classFile.AppendFormat("bool {0}", variavel);
+                    break;
+                case "STRING":
+                    classFile.AppendFormat("string {0}", variavel);
+                    break;
+                case "CARACTERE":
+                    classFile.AppendFormat("char {0}", variavel);
+                    break;
+            }
 
-			return base.VisitListaVar(context);
-		}
+            int r = context.ID().Length;
 
-		//In progress...
-		public override int VisitBlocos([NotNull] PortugolParser.BlocosContext context)
-		{
-			string blocos = context.GetText();
-			int tst = context.ChildCount;
+            for (int i = 1; i < r; i++)
+            {
+                variavel = context.ID(i).ToString();
+                classFile.AppendFormat(", {0}", variavel);
+            }
 
-			if (blocos.Contains("ESCOLHA"))
-			{
-				string varSwitch = context.@switch().valor(0).GetText();
-				classFile.Append($"{Identacao.CorpoMetodo}switch({varSwitch}){{{Environment.NewLine}");
+            classFile.Append(";\n");
 
-				string caso;
-				string sttm;
-				for (int i = 1; i < context.@switch().valor().Length; i++)
-				{
-					caso = context.@switch().valor(i).GetText();
-					classFile.Append($"{Identacao.CorpoMetodo}case{caso}:{Environment.NewLine}");
+            return base.VisitListaVar(context);
+        }
 
-					sttm = context.@switch().statement(i - 1).GetText();
-					if (sttm.Contains("SE"))
-					{
-						classFile.Append($"{Identacao.CorpoMetodo}if(){{{Environment.NewLine}");
-					}
-					else
-					{
-						classFile.Append($"{Identacao.CorpoMetodo}{sttm}{Environment.NewLine}");
-					}
-					classFile.Append($"{Identacao.CorpoMetodo}break;{Environment.NewLine}");
-				}
+        //In progress...
+        public override int VisitBlocos([NotNull] PortugolParser.BlocosContext context)
+        {
+            string blocos = context.GetText();
+            int tst = context.ChildCount;
 
-				
-			}
+            if (blocos.Contains("ESCOLHA"))
+            {
+                string varSwitch = context.@switch().valor(0).GetText();
+                classFile.Append($"{Identacao.CorpoMetodo}switch({varSwitch}){{{Environment.NewLine}");
 
-			
+                string caso;
+                string sttm;
+                for (int i = 1; i < context.@switch().valor().Length; i++)
+                {
+                    caso = context.@switch().valor(i).GetText();
+                    classFile.Append($"{Identacao.CorpoMetodo}case{caso}:{Environment.NewLine}");
 
-			return base.VisitBlocos(context);
-		}
+                    sttm = context.@switch().statement(i - 1).GetText();
+                    if (sttm.Contains("SE"))
+                    {
+                        classFile.Append($"{Identacao.CorpoMetodo}if(){{{Environment.NewLine}");
+                    }
+                    else
+                    {
+                        classFile.Append($"{Identacao.CorpoMetodo}{sttm}{Environment.NewLine}");
+                    }
+                    classFile.Append($"{Identacao.CorpoMetodo}break;{Environment.NewLine}");
+                }
 
 
-	}
+            }
+
+
+
+            return base.VisitBlocos(context);
+        }
+
+
+    }
 }
